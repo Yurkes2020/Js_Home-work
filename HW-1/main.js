42a408ed7143cc7db1d93e1bb103ac3cdff99f58
Yurkes2020 <yurkes2020@gmail.com>
2025-02-04 21:06:14 +0200

    hw - 1

---
+++ HW-1/main.js
@@ -0,0 +1,61 @@
+// --------------------------------------------
+
+let a = 'hello';
+console.log(a)
+
+let b = 'owu';
+console.log(b)
+
+let c = 'com';
+console.log(c)
+
+let d = 'ua';
+console.log(d)
+
+let e = 1;
+console.log(e)
+
+let f = 10;
+console.log(f)
+
+let g = -999;
+console.log(g)
+
+let j = 123;
+console.log(j)
+
+let h = 3.14;
+console.log(h)
+
+let k = 16;
+console.log(k)
+
+let l = true;
+console.log(l)
+
+let p = false;
+console.log(p)
+
+// --------------------------------------------
+
+let firstName = 'Yurii';
+let middleName = 'Mykolayovych';
+let lastName = 'Shulha';
+let person = firstName + ' ' + middleName + ' ' + lastName;
+console.log(person)
+
+// --------------------------------------------
+
+let number = 100;
+let string = '100';
+let boolean = true;
+console.log(typeof string, typeof number, typeof boolean);
+
+// --------------------------------------------
+
+let name = prompt('What is your name?');
+let nameMiddle = prompt('What is your middle name?');
+let age = prompt('What is your age number?');
+console.log(`${name} ${nameMiddle} - ${age} years old`);
+
+
