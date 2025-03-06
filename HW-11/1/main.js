1a33750193279fe45c9de10a0a7e44f1cdfb92f4
Yurkes2020 <yurkes2020@gmail.com>
2025-03-06 21:07:31 +0200

    hw - 11.2

--- HW-11/1/main.js
+++ HW-11/1/main.js
@@ -1,8 +1,6 @@
 // #HmvAfRQM
 // - взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
-//
-// 	#whXxOBlYS0H
-// 	- взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+
 
 const getCarts = async () => {
 	try {
---
+++ HW-11/2/main.js
@@ -0,0 +1,63 @@
+// #whXxOBlYS0H
+// - взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
+
+
+const getRecipes = async () => {
+	try {
+		const res = await fetch('https://dummyjson.com/recipes');
+		if (!res.ok) throw new Error(`Помилка: ${res.status}`);
+		const {recipes} = await res.json();
+		return recipes;
+	} catch (err) {
+		console.error('Помилка при отриманні даних:', err);
+		return null;
+	}
+};
+
+
+const renderCarts = async () => {
+	let recipes = await getRecipes();
+
+	if (!recipes) {
+		document.body.innerHTML = '<p style="color: red;">Не вдалося завантажити кошики. Спробуйте ще раз.</p>';
+		return;
+	}
+
+	recipes.forEach(recipe => {
+		let wrap = document.createElement('div');
+		let h2 = document.createElement('h2');
+		let div = document.createElement('div');
+		let img = document.createElement('img');
+		let ul = document.createElement('ul');
+		let ol = document.createElement('ol');
+
+		wrap.className = 'list';
+
+		h2.innerText = recipe.name;
+		img.setAttribute('src', recipe.image);
+
+		document.body.appendChild(wrap)
+		wrap.append(h2, div, ul)
+		div.append(img, ol)
+		let {ingredients, instructions} = recipe;
+
+		ingredients.forEach(ingredient => {
+
+			let liUL = document.createElement('li');
+
+			liUL.innerText = ingredient;
+			ul.appendChild(liUL);
+
+
+		})
+
+		instructions.forEach(instruction => {
+			let liOL = document.createElement('li');
+			liOL.innerText = instruction;
+			ol.appendChild(liOL);
+		})
+	})
+}
+
+renderCarts()
+
