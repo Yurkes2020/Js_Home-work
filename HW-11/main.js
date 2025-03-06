e2f128dac5dda7a8a484a09e76a96bc9b9c4161e
Yurkes2020 <yurkes2020@gmail.com>
2025-03-06 19:00:57 +0200

    hw - 11.1

--- HW-11/main.js
+++ HW-11/main.js
@@ -4,9 +4,30 @@
 // 	#whXxOBlYS0H
 // 	- взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути список під час відображення.
 
-fetch('https://dummyjson.com/carts')
-	.then(res => res.json())
-	.then(data => data)
-	.catch(err => console.log(err));
+const carts = async () => {
+	 return await  fetch('https://dummyjson.com/carts')
+		.then(res => res.json())
+		.then(data => data)
+		.catch(err => console.log(err));
+}
 
 
+carts().then(({carts}) => {
+
+	carts.map(({products}) => {
+		for (let product of products) {
+
+			let div = document.createElement('div');
+			let title = document.createElement('h2');
+			let text = document.createElement('p');
+			let img = document.createElement('img');
+
+			title.innerText = product.title;
+			text.innerText = product.price;
+			img.setAttribute('src', product.thumbnail);
+
+			document.body.appendChild(div);
+			div.append(title,img,text);
+		}
+	})
+})
